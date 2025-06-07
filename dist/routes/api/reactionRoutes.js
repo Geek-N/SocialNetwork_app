64633e69b273839dca52086602af929bce5a9497
Nina W <nina@Ninas-Air.lan>
2025-06-07 15:07:10 -0700

    new updated code

--- dist/routes/api/reactionRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { addReaction, deleteReaction } from '../../controllers/reactionController.js';
-const router = Router({ mergeParams: true });
-// /api/thoughts/:thoughtId/reactions
-router.route('/:thoughtId/reactions').post(addReaction);
-// /api/thoughts/:thoughtId/reactions/:reactionId
-router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
-export { router as reactionRouter };
